00a8166247ee58d8658e442bad942cefe007bb32
kmtwu <mr.kwu13579@gmail.com>
2024-05-30 17:35:15 -0400

    done

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 10.27;
+        double summerCost = 12.75;
+        double fallCost = 21.98;
+        double winterCost = 24.75;
+        double totalCost;
+        System.out.println("Your Spring cost is " + springCost + ".");
+        System.out.println("Your Summer cost is " + summerCost + ".");
+        System.out.println("Your Fall cost is " + fallCost + ".");
+        System.out.println("Your Winter cost is " + winterCost + ".");
+        totalCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Your total cost is " + totalCost + ".");
+    }
+}
